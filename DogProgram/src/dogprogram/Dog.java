d49310c5bb8d6994cc01ae179940ab29b5f3342d
bcroman <126107298+bcroman@users.noreply.github.com>
2024-01-23 13:47:03 +0000

    Added Methods

--- DogProgram/src/dogprogram/Dog.java
+++ DogProgram/src/dogprogram/Dog.java
@@ -4,8 +4,59 @@ public class Dog {
 
     //Instance Variables
     String name;
+    String colourOfCoat;
+    String breed;
+    String size;
+    String ownerName;
+    String foodType;
     char gender;
+    int noOfLegs;
     int age;
-    
+    double weight;
+    double height;
+    double distanceWalked;
+    boolean trained;
+    boolean isHungry;
+    boolean teethDirty;
+    boolean isHappy;
+
+    //Methods
+    public void bark() {
+        System.out.println("Woof Woof");
+    }
+
+    public void displayDeatils() {
+        System.out.println("Deatails for " + name);
+        System.out.println("----------------------------");
+        System.out.println("Gender: " + gender);
+        System.out.println("Age: " + age);
+        System.out.println("Coat Colour: " + colourOfCoat);
+        System.out.println("Breed: " + breed);
+        System.out.println("Size: " + size);
+    }
+
+    public void walkTheDog(double distanceWalked) {
+        isHappy = true;
+        isHungry = true;
+        System.out.println(name + " is happy");
+    }
+
+    public void patTheDog() {
+        isHappy = true;
+        System.out.println("Dog is happy because it has been pated");
+    }
+
+    public void cleanTheTeeth() {
+        teethDirty = true;
+        isHappy = false;
+        System.out.println("Dog's teeth is dirty");
+    }
+
+    public void feedTheDog() {
+        teethDirty = false;
+        isHungry = false;
+        System.out.println("Plase feed the dog");
+
+    }
 
 }
--- DogProgram/src/dogprogram/DogProgram.java
+++ DogProgram/src/dogprogram/DogProgram.java
@@ -4,25 +4,19 @@ public class DogProgram {
 
     public static void main(String[] args) {
         Dog theDog = new Dog();
-        Dog otherDog = new Dog();
 
         theDog.name = "Max";
         theDog.gender = 'M';
         theDog.age = 5;
-        
-        otherDog.name = "Milo";
-        otherDog.gender = 'M';
-        otherDog.age = 3;
 
-        System.out.println("Deatails for " + theDog.name);
-        System.out.println("----------------------------");
-        System.out.println("Gender: " + theDog.gender);
-        System.out.println("Age: " + theDog.age);
+        theDog.displayDeatils();
+        theDog.bark();
         
-        System.out.println("Deatails for " + otherDog.name);
-        System.out.println("----------------------------");
-        System.out.println("Gender: " + otherDog.gender);
-        System.out.println("Age: " + otherDog.age);
+        theDog.walkTheDog(0);
+        theDog.patTheDog();
+        theDog.cleanTheTeeth();
+        theDog.feedTheDog();
+
     }
 
 }
